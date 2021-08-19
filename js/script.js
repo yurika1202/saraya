f9d2817bc036495245d6c56dd82584e82c3a6708
yurika <yrpn120293@gmail.com>
2021-08-19 13:14:55 +0900

    [Add]top-レスポンシブ

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+/* global-nav_sp
+--------------------------------- */
+$(".global-nav__sp-btn").click(function (e) {
+    $(this).toggleClass('js-open_btn');
+    $(".open-contents_sp").toggleClass('js-open_nav');
+});
+
+$(".global-nav__link_sp").click(function (e) {
+    $(".global-nav__sp-btn").removeClass('js-open_btn');
+    $(".open-contents_sp").removeClass('js-open_nav');
+});
+
 /* under-header
 --------------------------------- */
 $(".under-header__nav-btn").click(function (e) {
