2d3cb15d01fb4c695c808f1cb442d63e3cc3b9b6
yurika <yrpn120293@gmail.com>
2021-08-22 06:45:03 +0900

    [Add]js-scroll

--- js/script.js
+++ js/script.js
@@ -7,27 +7,32 @@ const navBtn = document.getElementsByClassName('js_navBtn');
 btn.addEventListener('click', () => {
     btn.classList.toggle('js_open-btn');
     nav.classList.toggle('js_open-nav');
-})
+});
 
 for(let i = 0; i < navBtn.length; i++) {
     navBtn[i].addEventListener('click', () => {
         btn.classList.remove('js_open-btn');
         nav.classList.remove('js_open-nav');
-    })
+    });
 }
 
 /* スクロール
 --------------------------------- */
-$('.nav__item a[href^="#"]').click(function() {
-    var elHash = $(this).attr('href');
-    var pos = $(elHash).offset().top-83;
-    $('body,html').animate({scrollTop: pos}, 500);
-    return false;
-  });
+const scrollLink = document.querySelectorAll('a[href^="#"]');
 
-  $('.nav__items a[href^="#"]').click(function() {
-      var elHash = $(this).attr('href');
-      var smPos = $(elHash).offset().top-100;
-      $('body,html').animate({scrollTop: smPos}, 500);
-      return false;
-  })
+for(let i = 0; i < scrollLink.length; i++) {
+    scrollLink[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        let elHash = scrollLink[i].getAttribute('href');
+        let el = document.getElementById(elHash.replace('#', ''));
+        const rect = el.getBoundingClientRect().top;
+        const offset = window.pageYOffset;
+        const gap = 150;
+        const target = rect + offset - gap;
+
+        window.scrollTo({
+            top: target,
+            behavior: 'smooth'
+        });
+    });
+}
